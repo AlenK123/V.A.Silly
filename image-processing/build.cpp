0057178d2987ebde7abdf679decf3f823eb2d094
AlenK123 <alen3400@gmail.com>
2019-04-21 19:59:13 +0300

    starting to integrate the selective search algorithm

--- image-processing/build.cpp
+++ image-processing/build.cpp
@@ -1,24 +1,60 @@
 #include <iostream>
-#include <string>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/opencv.hpp>
+#include <sys/types.h>
+#include <vector>
 
-int main(int argc, char ** argv) {
-	if (argc < 2) {
-		std::cerr << argv[0] << " <img path>" << std::endl;
-		return EXIT_FAILURE;
-	}
+#include "opencv2/ximgproc/segmentation.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
 
-	cv::Mat src;
-	cv::Mat dst;
-	src = cv::imread(argv[1]);
-	
-	cv::resize(src, dst, cv::Size(32, 32));
-	cv::resize(dst, dst, cv::Size(500, 500));
-	
-	cv::imshow("Image", dst);
-	cv::waitKey(0);
-	
-	return EXIT_SUCCESS;
+#define HIGHT 256
+
+using namespace cv::ximgproc::segmentation;
+using ssptr = cv::Ptr<SelectiveSearchSegmentation>;
+using rois = std::vector<cv::Rect>;
+
+rois find_regions_of_interest(cv::Mat &image, ssptr &ss);
+cv::Mat draw_rois(cv::Mat image, rois v);
+
+int main(int argc, char ** argv) { 
+    if (argc < 3) {
+        printf("%s <path> <n threads>" "\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    cv::setUseOptimized(true);
+
+    cv::setNumThreads(std::atoi(argv[2]));
+
+    cv::Mat input_im = cv::imread(argv[1]);
+
+    cv::resize(input_im, input_im, cv::Size((input_im.cols * HIGHT / input_im.rows), HIGHT));
+
+    ssptr ss = createSelectiveSearchSegmentation();
+
+    while (true) {
+        cv::imshow("output", draw_rois(input_im, find_regions_of_interest(input_im, ss)));
+        if (cv::waitKey() == 113) break;
+    }
+    return EXIT_SUCCESS;
+}
+
+rois find_regions_of_interest(cv::Mat& image, ssptr& ss) {
+    rois v;
+    ss->setBaseImage(image);
+    ss->switchToSelectiveSearchFast();
+    ss->process(v);
+
+    /* reduce the region of intrests to relevent regions using cool math cool science */
+
+    return v;
+}
+
+cv::Mat draw_rois(cv::Mat image, rois v) {
+    cv::Mat out(image.clone());
+    for(auto it : v) {
+            
+        rectangle(out, it, cv::Scalar(0, 255, 0));
+    }
+    return out;
 }
